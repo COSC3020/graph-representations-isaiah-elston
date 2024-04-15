8b2ea9eb1916f861e1152a72a4cb0bafa23b49d9
Isaiah Elston <ielston@uwyo.edu>
2024-04-14 19:41:41 -0600

    Testing the code on GitHub...

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+  if (adjMatrix.length === 0) {
+    return [];
+  }
+
+  let nodeCount = adjMatrix.length;
+
+  let adjList = [];
+
+  for (let index = 0; index < nodeCount; index++) {
+    adjList[index] = [];
+  }
+
+  for (let list = 0; list < nodeCount; list++) {
+    for (let element = 0; element < nodeCount; element++) {
+      if (adjMatrix[list][element] === 1) {
+        adjList[list].push(element);
+      }
+    }
+  }
+
+  return adjList;
 }
