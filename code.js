408c878ca037e5280782c0f7a042993ccd5eb8c2
Isaiah Elston <ielston@uwyo.edu>
2024-04-15 10:26:24 -0600

    Added better variable names & the bonus exercise code...

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function convertToAdjList(adjMatrix) {
-  if (adjMatrix.length === 0) {
+  if (adjMatrix.length <= 0) {
     return [];
   }
 
@@ -7,17 +7,41 @@ function convertToAdjList(adjMatrix) {
 
   let adjList = [];
 
-  for (let index = 0; index < nodeCount; index++) {
-    adjList[index] = [];
+  for (let list = 0; list < nodeCount; list++) {
+    adjList[list] = [];
   }
 
   for (let list = 0; list < nodeCount; list++) {
     for (let element = 0; element < nodeCount; element++) {
-      if (adjMatrix[list][element] === 1) {
+      if (adjMatrix[list][element] == 1) {
         adjList[list].push(element);
       }
     }
   }
 
   return adjList;
+}
+
+// Bonus Exercise Code
+
+function convertToAdjMatrix(adjList) {
+  if (adjList.length == 0) {
+    return [];
+  }
+
+  let nodeCount = adjList.length;
+
+  let adjMatrix = [];
+  for (let list = 0; list < nodeCount; list++) {
+    adjMatrix[list] = [];
+  }
+
+  for (let list = 0; list < nodeCount; list++) {
+    for (let element = 0; element < adjList[list].length; element++) {
+      let nextElement = adjList[list][element];
+      adjMatrix[list].push(nextElement);
+    }
+  }
+
+  return adjMatrix;
 }
